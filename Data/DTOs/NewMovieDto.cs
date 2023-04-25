f6405da5aa2ad29743f48a30477ae2d3b77c4c77
Matt DeGoey <degoeym@gmail.com>
2023-04-25 15:36:21 +0000

    Add DTOs and profile mappings.

---
+++ Data/DTOs/NewMovieDto.cs
@@ -0,0 +1,30 @@
+using Data.Models;
+using FluentValidation;
+
+namespace Data.DTOs;
+
+public record NewMovieDto
+{
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public Rating Rating { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public DateTime InventoryDate { get; set; }
+
+    public NewMovieDto()
+    {
+        
+    }
+}
+
+public class NewMovieDtoValidator : AbstractValidator<NewMovieDto>
+{
+    public NewMovieDtoValidator()
+    {
+        RuleFor(n => n.Title).NotEmpty();
+        RuleFor(n => n.Description).NotEmpty().MaximumLength(250).NotEqual(n => n.Title);
+        RuleFor(n => n.Rating).NotEmpty();
+        RuleFor(n => n.ReleaseDate).NotEmpty();
+        RuleFor(n => n.InventoryDate).NotEmpty();
+    }
+}
---
+++ Profile.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using Data.DTOs;
+using Data.Models;
+
+public class MovieCollectionProfile : Profile
+{
+    public MovieCollectionProfile()
+    {
+        CreateMap<NewMovieDto, Movie>()
+            .AfterMap((src, dest) => 
+                {
+                    dest.InventoryDate = DateTime.SpecifyKind(src.InventoryDate, DateTimeKind.Utc);
+                    dest.ReleaseDate = DateTime.SpecifyKind(src.ReleaseDate, DateTimeKind.Utc);
+                });
+
+        CreateMap<UpdateMovieDto, Movie>()
+            .AfterMap((src, dest) => 
+                {
+                    dest.InventoryDate = DateTime.SpecifyKind(src.InventoryDate, DateTimeKind.Utc);
+                    dest.ReleaseDate = DateTime.SpecifyKind(src.ReleaseDate, DateTimeKind.Utc);
+                });
+    }
+}
